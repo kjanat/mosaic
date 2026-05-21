2e4d9258d83fe5b04c294941339987be1350101a
Kaj Kowalski <info@kajkowalski.nl>
2026-05-21 21:07:36 +0200

    fix(zed-mosaic): load Mosaic language in Zed
    Declare the language in the extension manifest and remove bracket query
    patterns for delimiters that the grammar tokenizes inside larger nodes, so
    Zed can register Mosaic and highlight `.mos` files.

--- crates/zed-mosaic/languages/mosaic/brackets.scm
+++ crates/zed-mosaic/languages/mosaic/brackets.scm
@@ -5,15 +5,6 @@
 ("{" @open "}" @close)
 ("<" @open ">" @close)
 
-(("\"" @open "\"" @close)
-  (#set! rainbow.exclude))
-
-(("'" @open "'" @close)
-  (#set! rainbow.exclude))
-
-(("`" @open "`" @close)
-  (#set! rainbow.exclude))
-
 ("$" @open "$" @close)
 ("*" @open "*" @close)
 ("**" @open "**" @close)
